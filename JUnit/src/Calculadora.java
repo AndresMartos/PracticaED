df67061acc331188226c3fe25dca936cede6999e
AndresMartos <98174192+AndresMartos@users.noreply.github.com>
2022-05-29 17:27:17 +0200

    JavaDoc del JUnit
    JavaDoc para el projecto de JUnit

--- JUnit/src/Calculadora.java
+++ JUnit/src/Calculadora.java
@@ -1,9 +1,24 @@
-
+/**
+ * Clase sobre la que se va ha aplicar el test de JUnit.
+ * @author Andrés, Diego, Carlos
+ *
+ */
 public class Calculadora {
+	/**
+	 * Método que suma dos números.
+	 * @param a
+	 * @param b
+	 * @return a+b
+	 */
 	public int sumar(int a, int b) {
 		return a+b;
 	}
-	
+	/**
+	 * Método que resta dos números.
+	 * @param a
+	 * @param b
+	 * @return a-b
+	 */
 	public int resta(int a, int b) {
 		return a-b;
 	}
--- JUnit/src/TestCalculadora.java
+++ JUnit/src/TestCalculadora.java
@@ -1,12 +1,20 @@
 import junit.framework.TestCase;
-
+/**
+ * Clase que testea la calculadora para ver que hace bien sus métodos.
+ * @author Andrés, Diego, Carlos
+ *
+ */
 public class TestCalculadora extends TestCase{
-	
+	/**
+	 * Método que comprueba que la suma esté bien.
+	 */
 	public void testSuma() {
 		Calculadora calculadora = new Calculadora();
 		assertTrue(calculadora.sumar(2, 2)==(2+2));
 	}
-	
+	/**
+	 * Método que comprueba que la resta esté bien.
+	 */
 	public void testResta() {
 		Calculadora calculadora = new Calculadora();
 		assertTrue(calculadora.resta(2, 2)==0);
